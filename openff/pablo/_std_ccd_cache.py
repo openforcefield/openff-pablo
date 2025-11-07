1872e8125f788ed0844631b20570faed75e0e228
Josh Mitchell <yoshanuikabundi@gmail.com>
2025-11-07 12:11:02 +1100

    Add Maestro-style NME cap (NMA)

--- openff/pablo/_std_ccd_cache.py
+++ openff/pablo/_std_ccd_cache.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from openff.pablo.chem import PEPTIDE_BOND
 from openff.pablo.residue import ResidueDefinition
 
 from .ccd._ccdcache import CcdCache
@@ -69,6 +70,33 @@ STD_CCD_CACHE: CcdCache = CcdCache(
                 {1: "O", 2: "H1", 3: "H2"},
                 "WAT",
             ).with_synonyms({"H1": ["1H"], "H2": ["2H"]}),
+        ],
+        # Maestro NME:
+        "NMA": [
+            ResidueDefinition.from_smiles(
+                residue_name="NMA",
+                mapped_smiles="[H:3][N:1]([H:4])[C:2]([H:5])([H:6])[H:7]",
+                atom_names={
+                    1: "N",
+                    2: "C",
+                    3: "HN1",
+                    4: "HN2",
+                    5: "H1",
+                    6: "H2",
+                    7: "H3",
+                },
+                leaving_atoms=[3],
+                linking_bond=PEPTIDE_BOND,
+                description="METHYLAMINE (MAESTRO)",
+            ).with_synonyms(
+                {
+                    "HN2": ["H"],
+                    "C": ["CA", "CH3"],
+                    "H1": ["1HH3", "HA1", "1HA"],
+                    "H2": ["2HH3", "HA2", "2HA"],
+                    "H3": ["3HH3", "HA3", "3HA"],
+                },
+            ),
         ],
     },
 )
